70779fc9b9dfacf3c946c06d9587b959a0002ef8
AshkenSC <393940378@qq.com>
2018-05-12 23:51:48 +0800

    Decide class of a mixed subset division
    A function, makorityCnt, is added to decide class of a subset according to the majority if it is the end of division but still mixed.

--- trees.py
+++ trees.py
@@ -55,3 +55,11 @@ def chooseBestFeatureToSplit(dataSet):
             bestInfoGain = infoGain         #if better than current best, set to best
             bestFeature = i
     return bestFeature                      #returns an integer
+
+def majorityCnt(classList):
+    classCount={}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
