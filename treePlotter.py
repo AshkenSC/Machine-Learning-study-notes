f55edab80824d9f94491c99b1038a4edea2fec11
AshkenSC <393940378@qq.com>
2018-05-15 00:02:52 +0800

    Finished prototype of plot creating function

--- treePlotter.py
+++ treePlotter.py
@@ -7,10 +7,14 @@ arrow_args = dict(arrowstyle='<-')
 
 # plot note with arrow
 def plotNode(nodeTxt, centerPt, parentPt, nodeType):
-    createPlot.ax1.a
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction', \
+                            xytext=centerPt, textCoords='axes fraction', \
+                            va='center', ha='center', bbox=nodeType, arrowprops=arrow_args)
 
 def createPlot():
     fig = plt.figure(1, facecolor='white')
     fig.clf()
     createPlot.ax1 = plt.subplot(111, frameon=False)
-    plotNode("decision node", (0.5, 0.1), (0.1, 0.5))
+    plotNode("decision node", (0.5, 0.1), (0.1, 0.5), decisionNode)
+    plotNode('leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
+    plt.show()
